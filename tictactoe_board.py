700b1783b9e680281a34cb514e848cf6e89ca9d2
Albert Skutnik <albert.skutnik@pw.edu.pl>
2023-12-19 01:25:04 +0100

    Implemented how class tictactoe board should work

--- tictactoe_board.py
+++ tictactoe_board.py
@@ -3,4 +3,23 @@ import numpy as np
 
 class TicTacToeBoard:
     def __init__(self) -> None:
+        """
+        Creates an instance of the starting board.
+        """
         self.board = np.full((3, 3, 9), " ", dtype="object")
+
+    # Player input, value is "x1, o2", and x, y coordinates
+    # Checks if move is possible
+    def move(self, value, x, y) -> bool:
+        pass
+
+    # Checks if cycle occures thanks to logic function
+    # writen in other file, if occures then data in
+    # array changes(cell of array becomes X or O in the center(4))
+    # and new data is tranfsfered to print board
+    def check_cycle_occures(self) -> bool:
+        pass
+
+    # Imports data to print board
+    def import_to_print(self) -> None:
+        pass
