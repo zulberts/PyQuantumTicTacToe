50e6a44f25bb4f61abffc96b42e4b4deadfe94c3
Albert Skutnik <skutnik13albert@gmail.com>
2023-12-29 13:51:07 +0100

    Added possibility of draw

--- wining_logic.py
+++ wining_logic.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-# dostajemy macierz 3x3x9 z np i transformuje
-def transform_matrix(input_matrix):
+# dostajemy macierz 3x3x9 z np i transformuje na 3x3
+def transform_matrix(input_matrix: np) -> np:
     new_matrix = np.full((3, 3), None)
     for i in range(3):
         for j in range(3):
@@ -11,17 +11,25 @@ def transform_matrix(input_matrix):
     return new_matrix
 
 
-# sprawdza kto wygral i zwraca albo str z X, O lub None jak nikt
-def check_win(matrix):
+# sprawdza kto wygral, moze wystapic tez remis
+# zwraca string kto wygral-
+def check_win(matrix: np) -> str:
+    matrix = transform_matrix(matrix)
+    win = []
     for i in range(3):
         if np.all(matrix[i, :] == 'X') or np.all(matrix[:, i] == 'X'):
-            return 'X'
+            win.append['X']
         if np.all(matrix[i, :] == 'O') or np.all(matrix[:, i] == 'O'):
-            return 'O'
+            win.append['O']
     normal_matrix = np.diag(matrix)
     flipped_matrix = np.diag(np.fliplr(matrix))
     if np.all(normal_matrix == 'X') or np.all(flipped_matrix == 'X'):
-        return 'X'
+        win.append['X']
     if np.all(normal_matrix == 'O') or np.all(flipped_matrix == 'O'):
-        return 'O'
-    return None
+        win.append['O']
+    if len(win) == 2:
+        return 'Gra kończy się remisem!'
+    elif len(win) == 1:
+        return 'Gre wygrywa {win[0]}!'
+    else:
+        return None
