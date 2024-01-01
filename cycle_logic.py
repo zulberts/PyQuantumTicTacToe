4871bdd0409fb385d1f3414758e19abb8fb4e90c
Albert Skutnik <albert.skutnik@pw.edu.pl>
2024-01-01 22:00:49 +0100

    Tested cycle_logic and implemented some changes in functions

--- cycle_logic.py
+++ cycle_logic.py
@@ -2,9 +2,68 @@ import numpy as np
 import random
 
 
-# I found an algorithm named DFS that could help me to
-# find if cycle occures, originally its used with graphs
-# but my graph is represented by array
+def pairs(array: np) -> dict:
+    entanglements = {
+        "x1": [],
+        "o2": [],
+        "x3": [],
+        "o4": [],
+        "x5": [],
+        "o6": [],
+        "x7": [],
+        "o8": [],
+        "x9": [],
+        "X": [],
+        "O": [],
+    }
+    for x in range(3):
+        for y in range(3):
+            for z in range(9):
+                cell = array[x, y, z]
+                if cell in entanglements:
+                    entanglements[cell].append((x, y, z))
+    return entanglements
+
+
+def transform_list(list_coordinates):
+    return [(x, y) for x, y, z, in list_coordinates]
+
+
+def pairs_entaglement(array: np) -> dict:
+    entaglements = pairs(array)
+    for key in list(entaglements.keys()):
+        entaglements[key] = transform_list(entaglements[key])
+        if len(entaglements[key]) < 2:
+            del entaglements[key]
+    entaglements.pop("X", None)
+    entaglements.pop("O", None)
+    return entaglements
+
+
+def create_adjacency_matrix(array):
+    adjacency_matrix_keys = {
+        (0, 0): 0,
+        (0, 1): 1,
+        (0, 2): 2,
+        (1, 0): 3,
+        (1, 1): 4,
+        (1, 2): 5,
+        (2, 0): 6,
+        (2, 1): 7,
+        (2, 2): 8,
+    }
+    adjacency_matrix = np.zeros((9, 9), dtype=int)
+    pairs_dict = pairs_entaglement(array)
+    for pairs in pairs_dict.values():
+        if len(pairs) == 2:
+            x, y = pairs
+            cell_x = adjacency_matrix_keys[x]
+            cell_y = adjacency_matrix_keys[y]
+            adjacency_matrix[cell_x, cell_y] = 1
+            adjacency_matrix[cell_y, cell_x] = 1
+    return adjacency_matrix
+
+
 def dfs(adjacency_matrix, start, current, visited, parent) -> bool:
     visited[current] = True
     for neighbor in range(len(adjacency_matrix)):
@@ -17,83 +76,42 @@ def dfs(adjacency_matrix, start, current, visited, parent) -> bool:
     return False
 
 
-# creates dictionary thanks to which can be used to check who won
-# creates dictionary thanks to which i can descride which pairs are
-# entagled
-def pairs(array: np) -> dict:
-    entaglements = {'x1': [], 'o2': [], 'x3': [], 'o4': [], 'x5': [],
-                    'o6': [], 'x7': [], 'o8': [], 'x9': [], 'X': [], 'O': []}
-    with np.nditer(array, flags=['multi_index']) as cells:
-        for cell in cells:
-            if cell.item() in entaglements:
-                entaglements[cell.item()].append(cells.multi_index)
-    return entaglements
-
-
-def pairs_entaglement(array: np) -> dict:
-    entaglements = pairs(array)
-    for key in list(entaglements.keys()):
-        if len(entaglements[key]) < 2:
-            del entaglements[key]
-    entaglements.pop('X', None)
-    entaglements.pop('O', None)
-    return entaglements
-
-
-def pairs_win(array: np) -> dict:
-    entaglements = pairs(array)
-    for key in list(entaglements.keys()):
-        if len(entaglements[key]) < 3:
-            del entaglements[key]
-    return entaglements
-
-
-# Chceck's if cycle occures
-def cycle(adjacency_matrix, start) -> bool:
+def cycle(array: np, start) -> bool:
+    adjacency_matrix = create_adjacency_matrix(array)
+    pairs_dict = pairs_entaglement(array)
+    values = list(pairs_dict.values())
+    for i in range(len(values)):
+        for j in range(i + 1, len(values)):
+            if values[i] == values[j]:
+                return True
     visited = [False] * len(adjacency_matrix)
-    # Creates list of vertices visited
     return dfs(adjacency_matrix, start, start, visited, None)
 
 
-# Creates array(macierz sąsiedztwa) in which cells entaglement occures
-def entaglement(array: np) -> np:
-    adjacency_matrix_keys = {
-        (0, 0): 0, (0, 1): 1, (0, 2): 2,
-        (1, 0): 3, (1, 1): 4, (1, 2): 5,
-        (2, 0): 6, (2, 1): 7, (2, 2): 8
-    }
-    adjacency_matrix = np.full((9, 9), 0, dtype=int)
-    dict_entaglements = pairs_entaglement(array)
-    for key in dict_entaglements.keys():
-        for cell_1, cell_2 in dict_entaglements[key]:
-            x = adjacency_matrix_keys[cell_1[:2]]
-            y = adjacency_matrix_keys[cell_2[:2]]
-            adjacency_matrix[x, y] = 1
-    return adjacency_matrix
-
-
-# function to if cycle occures
 def replace_character(box, prefer: bool):
-    if all(char == ' ' for char in box):
+    if all(char == " " for char in box):
         return box
-    elif all('x' in char or char == ' ' for char in box):
-        box[4] = 'X'
-    elif all('o' in char or char == ' ' for char in box):
-        box[4] = 'O'
-    elif any('x' in char for char in box) and any('o' in char for char in box):
-        box[4] = 'X' if prefer else 'O'
+    elif all("x" in char or char == " " for char in box):
+        box[4] = "X"
+    elif all("o" in char or char == " " for char in box):
+        box[4] = "O"
+    elif any("x" in char for char in box) and any("o" in char for char in box):
+        box[4] = "X" if prefer else "O"
     return box
 
 
-# function to if cycle occures
 def clear_box(box):
-    return [' ' if char not in ['X', 'O'] else char for char in box]
+    return [" " if char not in ["X", "O"] else char for char in box]
 
 
-# if cycle occures
 def modify_array(array: np):
-    mixed_boxes = [(i, j) for i in range(array.shape[0]) for j in range(array.shape[1])
-                   if any('x' in char for char in array[i, j]) and any('o' in char for char in array[i, j])]
+    mixed_boxes = [
+        (i, j)
+        for i in range(array.shape[0])
+        for j in range(array.shape[1])
+        if any("x" in char for char in array[i, j])
+        and any("o" in char for char in array[i, j])
+    ]
     random.shuffle(mixed_boxes)
     prefer = random.choice([True, False])
     for i in range(array.shape[0]):
@@ -102,7 +120,7 @@ def modify_array(array: np):
                 array[i, j] = replace_character(array[i, j], prefer)
                 prefer = not prefer
             else:
-                array[i, j] = replace_character(array[i, j])
+                array[i, j] = replace_character(array[i, j], prefer)
             array[i, j] = clear_box(array[i, j])
     return array
 
@@ -111,6 +129,6 @@ def impossible_list(array: np):
     coordinates_correct = []
     for row in range(3):
         for col in range(3):
-            if array[row][col][4] in ['X', 'O']:
+            if array[row][col][4] in ["X", "O"]:
                 coordinates_correct.append((row, col))
     return coordinates_correct
---
+++ test_cycle_logic.py
@@ -0,0 +1,418 @@
+import numpy as np
+from cycle_logic import pairs
+from cycle_logic import transform_list
+from cycle_logic import pairs_entaglement
+from cycle_logic import create_adjacency_matrix
+from cycle_logic import cycle
+from cycle_logic import replace_character
+from cycle_logic import clear_box
+from cycle_logic import modify_array
+from cycle_logic import impossible_list
+
+
+def test_pairs_empty_array():
+    empty_array = np.full((3, 3, 9), " ")
+    result_empty = pairs(empty_array)
+    for key in result_empty:
+        assert result_empty[key] == []
+
+
+def test_pairs_mixed_matrix():
+    array = np.array(
+        [
+            [
+                [" ", " ", " ", " ", "X", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", "O", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+        ]
+    )
+    result = pairs(array)
+    for key in result:
+        for pos in result[key]:
+            assert array[pos] == key
+
+
+def test_pairs_full_matrix():
+    matrix = np.full((3, 3, 9), "X")
+    result = pairs(matrix)
+    assert len(result["X"]) == 3 * 3 * 9
+    for key in result:
+        if key != "X":
+            assert len(result[key]) == 0
+
+
+def test_transform_list_empty():
+    assert transform_list([]) == []
+
+
+def test_transform_list_single():
+    assert transform_list([(1, 2, 3)]) == [(1, 2)]
+
+
+def test_transform_list_multiple():
+    input_list = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+    output_list = [(1, 2), (4, 5), (7, 8)]
+    assert transform_list(input_list) == output_list
+
+
+def test_pairs_entanglement_x1():
+    array = np.array(
+        [
+            [
+                ["x1", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                ["x1", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+        ]
+    )
+    result = pairs_entaglement(array)
+    assert "x1" in result
+    assert len(result["x1"]) == 2
+    assert result["x1"] == [(0, 0), (1, 0)]
+    assert "X" not in result
+    assert "O" not in result
+
+
+def test_pairs_entanglement_empty():
+    array = np.array(
+        [
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+        ]
+    )
+    result = pairs_entaglement(array)
+    assert result == {}
+    assert "X" not in result
+    assert "O" not in result
+
+
+def test_pairs_entanglement_x1_o2():
+    array = np.array(
+        [
+            [
+                ["x1", "o2", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                ["x1", "o2", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+        ]
+    )
+    result = pairs_entaglement(array)
+    assert "x1" in result
+    assert "o2" in result
+    assert len(result["x1"]) == 2
+    assert len(result["o2"]) == 2
+    assert result == {"x1": [(0, 0), (1, 0)], "o2": [(0, 0), (1, 0)]}
+    assert "X" not in result
+    assert "O" not in result
+
+
+def test_adjacency_matrix_empty():
+    array = np.array(
+        [
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+        ]
+    )
+    result = create_adjacency_matrix(array)
+    expected = np.zeros((9, 9), dtype=int)
+    return np.array_equal(result, expected)
+
+
+def test_adjacency_matrix_single_pair():
+    array = np.array(
+        [
+            [
+                ["x1", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                ["x1", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+        ]
+    )
+    result = create_adjacency_matrix(array)
+    expected = np.zeros((9, 9), dtype=int)
+    expected[0, 3] = 1
+    expected[3, 0] = 1
+    return np.array_equal(result, expected)
+
+
+def test_adjacency_matrix_multiple_pairs():
+    array = np.array(
+        [
+            [
+                ["x1", "o2", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                ["x1", "o2", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+        ]
+    )
+    result = create_adjacency_matrix(array)
+    expected = np.zeros((9, 9), dtype=int)
+    expected[0, 3] = 1
+    expected[3, 0] = 1
+    return np.array_equal(result, expected)
+
+
+def test_cycle_occures():
+    array = np.array(
+        [
+            [
+                ["x1", "o2", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                ["x1", "o2", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+        ]
+    )
+    assert cycle(array, 0) is True
+
+
+def test_cycle_occures_2():
+    array = np.array(
+        [
+            [
+                ["x1", "o2", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                ["x1", " ", "x3", " ", " ", " ", " ", " ", " "],
+                [" ", "o2", "x3", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+        ]
+    )
+    assert cycle(array, 0) is True
+
+
+def test_cycle_not_occures():
+    array = np.array(
+        [
+            [
+                ["x1", "o2", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                ["x1", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", "o2", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+        ]
+    )
+    assert cycle(array, 0) is False
+
+
+def test_replace_character_empty():
+    box = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    output = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    assert replace_character(box, True) == output
+
+
+def test_replace_character_all_x():
+    box = ["x", "x", "x", "x", " ", "x", "x", "x", "x"]
+    output = ["x", "x", "x", "x", "X", "x", "x", "x", "x"]
+    assert replace_character(box, True) == output
+
+
+def test_replace_character_all_o():
+    box = ["o", "o", "o", "o", " ", "o", "o", "o", "o"]
+    output = ["o", "o", "o", "o", "O", "o", "o", "o", "o"]
+    assert replace_character(box, True) == output
+
+
+def test_replace_character_mixed_prefer_x():
+    box = ["x", "o", " ", " ", " ", " ", "o", "x", " "]
+    output = ["x", "o", " ", " ", "X", " ", "o", "x", " "]
+    assert replace_character(box, True) == output
+
+
+def test_replace_character_mixed_prefer_o():
+    box = ["x", "o", " ", " ", " ", " ", "o", "x", " "]
+    assert replace_character(box, False) == [
+        "x",
+        "o",
+        " ",
+        " ",
+        "O",
+        " ",
+        "o",
+        "x",
+        " ",
+    ]
+
+
+def test_clear_box_empty():
+    box = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    assert clear_box(box) == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+def test_clear_box_with_X_O():
+    box = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+    assert clear_box(box) == ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+
+
+def test_clear_box_mixed():
+    box = ["x2", "O", "o3", " ", "X", "x1", "O", "x4", "o5"]
+    assert clear_box(box) == [" ", "O", " ", " ", "X", " ", "O", " ", " "]
+
+
+def test_modify_array_with_mixed_content():
+    array = np.array(
+        [
+            [
+                ["x1", " ", " ", " ", " ", " ", " ", " ", "o2"],
+                ["o2", " ", " ", " ", " ", " ", " ", " ", "x1"],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ]
+            for _ in range(3)
+        ],
+        dtype=object,
+    )
+    result = modify_array(array)
+    for i in range(3):
+        for j in range(3):
+            assert all(char in ["X", "O", " "] for char in result[i, j])
+
+
+def test_modify_array_with_all_empty():
+    array = np.full((3, 3, 9), " ", dtype=object)
+    result = modify_array(array)
+    for i in range(3):
+        for j in range(3):
+            assert all(char == " " for char in result[i, j])
+
+
+def test_modify_array_with_all_x():
+    array = np.full((3, 3, 9), "x1", dtype=object)
+    result = modify_array(array)
+    for i in range(3):
+        for j in range(3):
+            assert all(char in ["X", " "] for char in result[i, j])
+
+
+def test_impossible_list_with_XOs():
+    array = np.array(
+        [
+            [
+                [" ", " ", " ", " ", "X", " ", " ", " ", " "],
+                [" ", " ", " ", " ", "O", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", "X", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ],
+            [
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", "O", " ", " ", " ", " "],
+            ],
+        ],
+        dtype=object,
+    )
+    result = impossible_list(array)
+    output = [(0, 0), (0, 1), (1, 1), (2, 2)]
+    assert sorted(result) == sorted(output)
+
+
+def test_impossible_list_without_XOs():
+    array = np.full((3, 3, 9), " ", dtype=object)
+    result = impossible_list(array)
+    assert result == []
